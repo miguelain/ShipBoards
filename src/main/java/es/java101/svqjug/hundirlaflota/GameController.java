1889359ae4b1cd37cdaba968620000d5fbab4463
Lancool <Lancool@lancool.com>
2017-12-20 19:42:36 +0100

    Game Controller suggestions

--- src/main/java/es/java101/svqjug/hundirlaflota/GameController.java
+++ src/main/java/es/java101/svqjug/hundirlaflota/GameController.java
@@ -2,4 +2,11 @@ package es.java101.svqjug.hundirlaflota;
 
 public class GameController {
 
+    create player 1 , create board 1
+
+    create player 2 , create board 2
+
+    start game
+
+    show results
 }
