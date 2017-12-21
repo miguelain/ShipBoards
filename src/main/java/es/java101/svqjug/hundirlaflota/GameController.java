a4ac89e34aef347407f9cfaa869c2faf011c0384
Lancool <Lancool@lancool.com>
2017-12-21 22:28:41 +0100

    Now a player is asked for drawing an Enemy Board (y/n)
    Next ---> GameController its too busy.
    Next ---> Let´s regroup to clean code in a GameHelper

--- src/main/java/es/java101/svqjug/hundirlaflota/GameController.java
+++ src/main/java/es/java101/svqjug/hundirlaflota/GameController.java
@@ -2,8 +2,10 @@ package es.java101.svqjug.hundirlaflota;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class GameController {
+
     private List<Player> players = new LinkedList<>();
     private List<Board> boards = new LinkedList<>();
 
@@ -29,6 +31,7 @@ public class GameController {
     }
 
     public void gameIntro() {
+
         System.out.println("\t **** Welcome to this.GameOfShips \n\t **** Rules:");
         System.out.printf("\n\t **** In order to start a new Game Of Ships you will need 2 players" +
                 "\n\t **** In this ver 2.0 , each player has a random board build with 10 ships" +
@@ -36,6 +39,7 @@ public class GameController {
     }
 
     public void shootIntro() {
+
         System.out.println("\n\t **** Make shoots to sink enemy Ships");
         System.out.println("\n\t **** A shoot is made by hitting 2 positions");
         System.out.println("\n\t *** First a hit for x Axis \u2192 , then a hit for Y Axis \u2193 ");
@@ -47,6 +51,7 @@ public class GameController {
     }
 
     private void startGame(Player player1, Player player2) {
+
         Board board1 = player1.getBoard();
         Board board2 = player2.getBoard();
 
@@ -55,6 +60,35 @@ public class GameController {
         if (board1.canFireInCell(x, y)) {
             board1.fireInCell(x,y);
         }
+        if (askForDrawBoard().equalsIgnoreCase("y")) {
+            board2.drawBoard(player2);
+        }
+    }
+
+    public String askForDrawBoard() {
+
+        Scanner scanner = new Scanner(System.in);
+        String yesOrNot;
+        boolean yesWeDraw;
+
+        do {
+            System.out.println("\n\t **** Want to see your enemy Board? (answer y or n)");
+            yesOrNot = scanner.nextLine();
+            yesWeDraw = checkYesOrNot(yesOrNot);
+            if (!yesWeDraw) {
+                System.out.println("\n\t **** Please make sure your input is a YES(y) or Not (n), thanks");
+            }
+        } while (!yesWeDraw);
+
+        return yesOrNot;
+    }
+
+    public boolean checkYesOrNot(String yesOrNot) {
+
+        if(yesOrNot.equalsIgnoreCase("y") || (yesOrNot.equalsIgnoreCase("n"))) {
+            return true;
+        }
+        return false;
     }
 
 }
